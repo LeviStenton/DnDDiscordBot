7e12479054afc97f624d7ea2fee53f890e7e6d1d
Levi Stenton <levistenton@gmail.com>
2020-10-13 13:20:49 +1000

    Changed function to a die roller for now

--- bot.py
+++ bot.py
@@ -12,13 +12,15 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='test')
-async def TestFunction(ctx):
-    testing = [
-        'Hello there!','Hey!'
-    ]
-
-    response = random.choice(testing)
-    await ctx.send(response)
+@bot.command(name='roll')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = []
+    totalDice = 0
+    
+    for i in number_of_dice:
+        dice.insert(str(random.choice(range(1, number_of_sides + 1))))
+        totalDice += dice[i]
+    
+    await ctx.send('Dice: '+dice+' Total: '+totalDice)
 
 bot.run(TOKEN)
